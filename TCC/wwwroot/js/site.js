95f878782d5990100a130b2d31882785a68a920c
Bruno <jogar1203@gmail.com>
2023-08-10 09:41:02 -0300

    Smooth Scroll

--- TCC/wwwroot/js/site.js
+++ TCC/wwwroot/js/site.js
@@ -1,4 +1,32 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
 
-// Write your JavaScript code.
+function getDistanceFromTheTop(element) {
+    const id = element.getAttribute("href");
+    return document.querySelector(id).offsetTop;
+};
+
+function nativeScroll(distanceFromTheTop) {
+    window.scroll({
+        top: distanceFromTheTop,
+        behavior: "smooth",
+    })
+}
+
+function scrollToSection(event) {
+    event.preventDefault();
+    const distanceFromTop = getDistanceFromTheTop(event.target) ;
+    nativeScroll(distanceFromTop)
+}
+menuLinks.forEach((link) =>
+    link.addEventListener("click", scrollToSection)
+)
+
+const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime;
+    const newX = easeInOutQuart(time, startX, distanceX, duration);
+    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+        clearInterval(timer);
+    }
+    window.scroll(newX, newY);
+}, 1000 / 60);
