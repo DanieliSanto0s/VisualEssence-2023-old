90a008deb3ee24c1bcc1912a2da9ebafca01ffe2
Bruno <jogar1203@gmail.com>
2023-08-10 11:48:18 -0300

    +2

--- TCC/Controllers/HomeController.cs
+++ TCC/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace Tcc.Controllers
         {
             return View();
         }
+        public IActionResult Doacao()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
