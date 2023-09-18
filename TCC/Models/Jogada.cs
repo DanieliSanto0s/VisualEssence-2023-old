09746a0bbcc7d2562921ea9035e47316a6ab1a7b
Bruno <Bruno@DESKTOP-AA6HRQC>
2023-09-17 23:39:17 -0300

    +BANCO CONECTADO

--- TCC/Models/Jogada.cs
+++ TCC/Models/Jogada.cs
@@ -7,7 +7,6 @@ namespace Tcc.Models
         public Guid Id { get; set; }
         public DateTime DataJogou { get; set; }
         public int PontuacaoJogo { get; set; }
-
         public int IdCrianca { get; set; }
         public int IdJogo { get; set; }
     }
--- TCC/wwwroot/js/site.js
+++ TCC/wwwroot/js/site.js
@@ -21,3 +21,20 @@ menuLinks.forEach((link) =>
     link.addEventListener("click", scrollToSection)
 )
 
+//----------------------------------//
+
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function () {
+    this.classList.toggle('bi-brightness-high-fill');
+    if (this.classList.toggle('bi-moon')) {
+        body.style.backgroundImage = 'linear-gradient(180deg, rgba(16,121,199,1) 0%, rgba(16,121,204,0.9192051820728291) 51%, rgba(68,179,190,0.8519782913165266) 100%)'; // Gradiente Diurno
+        body.style.transition = '3s';
+    } else {
+        body.style.backgroundImage = 'linear-gradient(180deg, rgba(2,14,42,1) 0%, rgba(18,5,61,1) 54%, rgba(36,2,37,1) 100%)'; // Gradiente noturno
+        body.style.transition = '3s';
+    }
+   
+});
+
